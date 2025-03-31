97278242bad04f0e8f889441ff6b925a3567e827
Matúš Klement <matuskoklemo@gmail.com>
2025-03-31 21:42:25 +0200

    refactor: add inverted dependency on VatProvider instead of hard-coded value

--- src/domain/Invoice/AddInvoiceUseCase.php
+++ src/domain/Invoice/AddInvoiceUseCase.php
@@ -6,6 +6,8 @@ namespace App\domain\Invoice;
 
 use App\domain\Invoice\ExchangeRate\ExchangeRateProvider;
 use App\domain\Invoice\ExchangeRate\UnableToFetchExchangeRateException;
+use App\domain\Invoice\Vat\UnableToFetchVatException;
+use App\domain\Invoice\Vat\VatProvider;
 use Psr\Clock\ClockInterface;
 
 final readonly class AddInvoiceUseCase
@@ -14,21 +16,24 @@ final readonly class AddInvoiceUseCase
         private InvoiceRepository $invoiceRepository,
         private ExchangeRateProvider $exchangeRateProvider,
         private ClockInterface $clock,
+        private VatProvider $vatProvider,
     ) {
     }
 
     /**
      * @throws UnableToFetchExchangeRateException
+     * @throws UnableToFetchVatException
      */
     public function execute(array $invoice): array
     {
+        $issued_on = new \DateTimeImmutable($invoice['issued_on']);
         $invoice['exchange_rate'] = $this->exchangeRateProvider->fetch(
             'EUR',
             $invoice['currency'],
-            new \DateTimeImmutable($invoice['issued_on']),
+            $issued_on,
         );
 
-        $invoice['vat'] = 23.0;
+        $invoice['vat'] = $this->vatProvider->fetch($issued_on);
         $invoice['created_at'] = $this->clock->now()
             ->format('Y-m-d H:i:s');
 
